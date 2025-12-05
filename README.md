018807c33626a6b971909dae875358e59e6683c5
ColinThePanda <urboycolinthepanda@gmail.com>
2025-12-05 11:41:57 -0500

    Add family size analysis, better graphs, and a readme

--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # TitanicDataAnalysis
-CS2 Assignment
+
+![CS2 Assignment](https://img.shields.io/badge/CS2-Assignment-blue)
+[![Dataset](https://img.shields.io/badge/Titanic-Dataset-green)](titanic_data.csv)
+
+## Printed Info
+
+When the program is run, the entire dataset is displayed like this
+
+| PassengerId | Survived | Pclass | Name                                              | Sex    | Age | SibSp | Parch | Ticket            | Fare     | Cabin | Embarked | FamilySize |
+|-------------|----------|--------|---------------------------------------------------|--------|-----|-------|-------|-------------------|----------|-------|----------|------------|
+| 1           | 0        | 3      | Braund, Mr. Owen Harris                           | male   | 22  | 1     | 0     | A/5 21171         | 7.25     |       | S        | 2          |
+| 2           | 1        | 1      | Cumings, Mrs. John Bradley(Florence Briggs Thayer)| female | 38  | 1     | 0     | PC 17599          | 71.2833  | C85   | C        | 2          |
+| 3           | 1        | 3      | Heikkinen, Miss. Laina                            | female | 26  | 0     | 0     | STON/O2. 3101282  | 7.925    |       | S        | 1          |
+| 4           | 1        | 1      | Futrelle, Mrs. Jacques Heath(Lily May Peel)       | female | 35  | 1     | 0     | 113803            | 53.1     | C123  | S        | 2          |
+| 5           | 0        | 3      | Allen, Mr. William Henry                          | male   | 35  | 0     | 0     | 373450            | 8.05     |       | S        | 1          |
+| 6           | 0        | 3      | Moran, Mr. James                                  | male   |     | 0     | 0     | 330877            | 8.4583   |       | Q        | 1          |
+| 7           | 0        | 1      | McCarthy, Mr. Timothy J                           | male   | 54  | 0     | 0     | 17463             | 51.8625  | E46   | S        | 1          |
+| 8           | 0        | 3      | Palsson, Master. Gosta Leonard                    | male   | 2   | 3     | 1     | 349909            | 21.075   |       | S        | 5          |
+| 9           | 1        | 3      | Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg) | female | 27  | 0     | 2     | 347742            | 11.1333  |       | S        | 3          |
+| 10          | 1        | 2      | Nasser, Mrs. Nicholas(Adele Achem)                | female | 14  | 1     | 0     | 237736            | 30.0708  |       | C        | 2          |
+
+Additionally, other data values are dynamically computed and displayed like this
+
+``` txt
+Survival Rate: 38.4%
+Male Survival Rate: 18.9%
+Female Survival Rate: 74.2%
+Average Age: 23.8
+Average Age Amongst Survivors: 24.0
+Average Age Amongst Non-Survivors: 23.7
+Oldest Passenger: 80.0
+Youngest Passenger: 0.4
+First Class Survival Rate: 0.63%
+Second Class Survival Rate: 0.47%
+Third Class Survival Rate: 0.24%
+Highest Survival Rate Class: 1
+First Class Average Fare: $84.15
+Second Class Average Fare: $20.66
+Third Class Average Fare: $13.68
+Survival Rate Based on Family Size:
+         1. 30.4%
+         2. 55.3%
+         3. 57.8%
+         4. 72.4%
+         5. 20.0%
+         6. 13.6%
+         7. 33.3%
+         8. 0.0%
+         9. None Applicable
+        10. None Applicable
+        11. 0.0%
+It is better to travel with family
+```
+
+## Graph of Data
+
+This graph is shown using matplotlib when the program is run
+![Picture showing graphs displayed in program](graph_images.png)
+
+## Data Values
+
+Other data values are calculated and are written to an [analysis file](analysis.txt) that looks like this
+
+```yaml
+Titanic Data Analysis
+
+All Passengers:
+    Population: 891
+        Survivors: 342
+        Deceased: 549
+    Survival Rate: 38.4%
+Genders:
+    Male:
+        Population: 577
+            Survivors: 109
+            Deceased: 468
+        Survival Rate: 18.9%
+    Female:
+        Population: 314
+            Survivors: 233
+            Deceased: 81
+        Survival Rate: 74.2%
+Classes:
+    First Class:
+        Population: 216
+            Survivors: 136
+            Deceased: 80
+        Survival Rate: 63.0%
+    Second Class:
+        Population: 184
+            Survivors: 87
+            Deceased: 97
+        Survival Rate: 47.3%
+    Third Class:
+        Population: 491
+            Survivors: 119
+            Deceased: 372
+        Survival Rate: 24.2%
+Age Groups:
+    Child (<18):
+        Population: 113
+            Survivors: 61
+            Deceased: 52
+        Survival Rate: 54.0%
+    Adult (18-60):
+        Population: 575
+            Survivors: 222
+            Deceased: 353
+        Survival Rate: 38.6%
+    Seniors (>60):
+        Population: 575
+            Survivors: 222
+            Deceased: 353
+        Survival Rate: 38.6%
+Family Sizes:
+    1. 30.4%
+    2. 55.3%
+    3. 57.8%
+    4. 72.4%
+    5. 20.0%
+    6. 13.6%
+    7. 33.3%
+    8. 0.0%
+    9. None Applicable
+    10. None Applicable
+    11. 0.0%
+
+Highest Survival Rate Group:
+    Gender: Female
+    Class: First
+    Age Group: Child
+    Family Size: 4
+```
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
 import matplotlib.pyplot as plt
-from collections import namedtuple, Counter
+from collections import namedtuple
+from typing import List, Dict, Any, TypeVar
+
+T = TypeVar("T")
 
 """
 GCDS CS: Titanic Dataset Analysis
@@ -83,13 +86,13 @@ STARTER CODE TEMPLATE:
 
 TitanicData = namedtuple("TitanicData", ["passenger_id", "survived", "p_class", "name", "sex", "age", "sib_sp", "parch", "ticket", "fare", "cabin", "embarked", "family_size"])
 
-def read_data(file_name : str) -> list[TitanicData]:
+def read_data(file_name : str) -> List[TitanicData]:
     with open(file_name, "r") as file:
         reader = csv.reader(file)
         lines = [line for line in reader][1:]
         return [TitanicData(*line, family_size=str(int(line[6]) + int(line[7]) + 1)) for line in lines]
     
-def display_data(data : list[TitanicData]):
+def display_data(data : List[TitanicData]):
     data_len_map = {i : 0 for i, _ in enumerate(data[0])}
     header = ["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked", "FamilySize"]
     for line in data:
@@ -104,84 +107,87 @@ def display_data(data : list[TitanicData]):
             print(f"{value:^{data_len_map[i]}}", end="|")
         print()
 
-def survival_rate(data : list[TitanicData]) -> float:
+def survival_rate(data : List[TitanicData]) -> float:
     return sum([int(value.survived) for value in data]) / len(data)
 
-def survival_rate_gender(data : list[TitanicData]):
+def survival_rate_gender(data : List[TitanicData]):
     male_data = [person for person in data if person.sex == "male"]
     female_data = [person for person in data if person.sex == "female"]
     male_survival = survival_rate(male_data)
     female_survival = survival_rate(female_data)
     return male_survival, female_survival
 
-def valid_property(data : list[TitanicData], property : str):
+def valid_property(data : List[TitanicData], property : str):
     return [person for person in data if getattr(person, property) != ""]
 
-def average_age(data : list[TitanicData]):
+def average_age(data : List[TitanicData]):
     return sum([float(person.age) for person in valid_property(data, "age")]) / len(data)
 
-def average_age_survived(data : list[TitanicData]):
+def average_age_survived(data : List[TitanicData]):
     survivors = [person for person in data if person.survived == "1"]
     non_survivors = [person for person in data if person.survived == "0"]
     survivor_age = average_age(survivors)
     non_survivor_age = average_age(non_survivors)
     return survivor_age, non_survivor_age
 
-def oldest_passenger(data : list[TitanicData]):
+def oldest_passenger(data : List[TitanicData]):
     age = -1
     for person in valid_property(data, "age"):
         if float(person.age) > age:
             age = float(person.age)
     return age
 
-def youngest_passenger(data : list[TitanicData]):
+def youngest_passenger(data : List[TitanicData]):
     age = float("inf")
     for person in valid_property(data, "age"):
         if float(person.age) < age:
             age = float(person.age)
     return age
 
-def class_data(data : list[TitanicData], passenger_class : int):
+def class_data(data : List[TitanicData], passenger_class : int):
     class_recorded = valid_property(data, "p_class")
     return [person for person in class_recorded if person.p_class == str(passenger_class)]
 
-def class_survival(data : list[TitanicData], passenger_class : int):
+def class_survival(data : List[TitanicData], passenger_class : int):
     return survival_rate(class_data(data, passenger_class))
 
-def average_fare(data : list[TitanicData]):
+def average_fare(data : List[TitanicData]):
     return sum([float(person.fare) for person in data]) / len(data)
 
-def class_fare(data : list[TitanicData], passenger_class : int):
+def class_fare(data : List[TitanicData], passenger_class : int):
     return average_fare(class_data(data, passenger_class))
 
-def family_size_data(data : list[TitanicData], family_size : int):
+def family_size_data(data : List[TitanicData], family_size : int):
     return [person for person in data if int(person.family_size) == family_size]
 
-def family_size_survival(data : list[TitanicData], family_size : int):
+def family_size_survival(data : List[TitanicData], family_size : int):
     return survival_rate(family_size_data(data, family_size))
 
-def min_family_size(data : list[TitanicData]):
+def min_family_size(data : List[TitanicData]):
     family_size = float("inf")
     for person in data:
         if int(person.family_size) < family_size:
             family_size = int(person.family_size)
     return int(family_size)
 
-def max_family_size(data : list[TitanicData]):
+def max_family_size(data : List[TitanicData]):
     family_size = -1
     for person in data:
         if int(person.family_size) > family_size:
             family_size = int(person.family_size)
     return family_size
 
-def group_analysis(data : list[TitanicData], tabs = 1) -> str:
+def group_analysis(data : List[TitanicData], tabs = 1) -> str:
     analysis = f"{"\t" * tabs}Population: {len(data)}\n"
     analysis += f"{"\t" * (tabs + 1)}Survivors: {len([person for person in data if person.survived == "1"])}\n"
     analysis += f"{"\t" * (tabs + 1)}Deceased: {len([person for person in data if person.survived == "0"])}\n"
     analysis += f"{"\t" * tabs}Survival Rate: {survival_rate(data) * 100:.1f}%"
     return analysis
 
-def write_analysis(data : list[TitanicData], filepath : str):
+def write_analysis(data : List[TitanicData], filepath : str):
+    def name_max_map(map : Dict[T, Any]) -> T:
+        return list(map.keys())[list(map.values()).index(max(map.values()))]
+    
     with open(filepath, 'w') as file:
         file.write("Titanic Data Analysis\n\n")
         
@@ -221,23 +227,40 @@ def write_analysis(data : list[TitanicData], filepath : str):
         file.write("\n\tSeniors (>60):\n")
         file.write(group_analysis(seniors, tabs=2))
         
+        family_sizes = [int(person.family_size) for person in data]
+        family_size_map = dict()
+        
+        file.write("\nFamily Sizes:")
+        for i in range(min(family_sizes), max(family_sizes) + 1):
+            family_size_group = [person for person in data if int(person.family_size) == i]
+            if len(family_size_group) > 0:
+                rate = survival_rate(family_size_group)
+                file.write(f"\n\t{i}. {rate*100:.1f}%")
+                family_size_map[i] = rate
+            else:
+                file.write(f"\n\t{i}. None Applicable")
+        
         gender_map = {"Male" : survival_rate(males), "Female" : survival_rate(females)}
-        best_gender = list(gender_map.keys())[list(gender_map.values()).index(max(gender_map.values()))]
+        best_gender = name_max_map(gender_map)
         
         class_map = {"First" : survival_rate(first_class), "Second" : survival_rate(second_class), "Third" : survival_rate(third_class)}
-        best_class = list(class_map.keys())[list(class_map.values()).index(max(class_map.values()))]
+        best_class = name_max_map(class_map)
         
         age_group_map = {"Child" : survival_rate(children), "Adult" : survival_rate(adults), "Senior" : survival_rate(seniors)}
-        best_age_group = list(age_group_map.keys())[list(age_group_map.values()).index(max(age_group_map.values()))]
+        best_age_group = name_max_map(age_group_map)
+        
+        best_family_size = name_max_map(family_size_map)
         
         file.write("\n\nHighest Survival Rate Group:")
         file.write(f"\n\tGender: {best_gender}")
         file.write(f"\n\tClass: {best_class}")
         file.write(f"\n\tAge Group: {best_age_group}")
+        file.write(f"\n\tFamily Size: {best_family_size}")
 
-def plot_data(data : list[TitanicData]):
+def plot_data(data : List[TitanicData]):
     male, female = survival_rate_gender(data)
-    valid_int_ages = [round(float(person.age)) for person in valid_property(data, "age")]
+    valid_int_ages = [int(float(person.age)) for person in valid_property(data, "age")]
+    mens_ages = [int(float(person.age)) for person in valid_property(data, "age") if person.sex == "male"]
     first_survival_rate = class_survival(data, 1)
     second_survival_rate = class_survival(data, 2)
     third_survival_rate = class_survival(data, 3)
@@ -250,8 +273,9 @@ def plot_data(data : list[TitanicData]):
     ax1.set_ylim(0, 100)
     ax1.set_title("Survival Rate of Genders on the Titanic")
     
-    ax2.hist(valid_int_ages, bins=50, edgecolor="black")
+    ax2.hist(valid_int_ages, bins=max(valid_int_ages) - min(valid_int_ages), edgecolor="black")
     ax2.set_xlabel("Ages (yrs)")
+    ax2.set_ylabel("Number of People")
     ax2.set_title("Age Distribution on the Titanic")
     
     ax3.bar(["First Class", "Second Class", "Third Class"], [first_survival_rate * 100, second_survival_rate * 100, third_survival_rate * 100], color=["navy", "seagreen", "darkorange"])
